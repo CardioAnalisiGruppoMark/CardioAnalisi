e58400da9b38de0ffbc2fb384f44cd728963ec66
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-21 13:13:24 +0100

    Add project files.

---
+++ DataCardio.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCardio;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCardio()
+        {
+            double etàsoggetto = 20;
+            double risaspettato = 200;
+            double riscalcolato = DataCardio.
+        }
+    }
+}
