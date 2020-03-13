6ff6b371bf695a58e1c697111f234834c641c093
CardioAnalisiGruppoMark <60812690+CardioAnalisiGruppoMark@users.noreply.github.com>
2020-03-13 19:41:57 +0100

    Update Class1.cs

--- CardioLibrary/Class1.cs
+++ CardioLibrary/Class1.cs
@@ -1,8 +1,40 @@
-﻿using System;
+
+using System;
 
 namespace CardioLibrary
 {
-    public class Class1
+    public class DataCardio
     {
+        public double CalcoloMaxBattitiCardiaci(int eta)
+        {
+            double maxbattiti = 0;
+            maxbattiti= 220 - eta;
+            return maxbattiti;
+        }
+        public double CalcoloMaxBattitiPalestra(int eta)
+        {
+            double maxbattiti = 0;
+            double maxbattitipalestra = 0;
+            maxbattiti = 220 - eta;
+            maxbattitipalestra = (90 * maxbattiti) / 100;
+            return maxbattitipalestra;
+        }
+        public string Battiti_Riposo(double battiti)
+        {
+            string risposta ="";
+            if(battiti<60)
+            {
+                risposta = "Bradicardia";
+            }
+            if(battiti>=60 && battiti<=100)
+            {
+                risposta = "Battiti nella norma";
+            }
+            if(battiti>100)
+            {
+                risposta = "Tachicardia";
+            }
+
+        }
     }
 }
